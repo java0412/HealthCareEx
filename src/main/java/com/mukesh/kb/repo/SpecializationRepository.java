99df5250f9aa3a225ef8494d1f5a560bf62d8ab0
java0412 <mayankbindkb@gmail.com>
2021-09-14 14:20:27 +0530

    repo commit

---
+++ src/main/java/com/mukesh/kb/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package com.mukesh.kb.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mukesh.kb.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
